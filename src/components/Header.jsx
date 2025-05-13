1a11a44ac2708ebd2b0b246e6e3a6e335adab5ce
DevHanza <hansana123dasanayake@gmail.com>
2025-05-13 15:27:31 +0530

    Build the Header: Desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,27 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="h-22 flex items-center justify-center">
+      <nav>
+        <ul className="flex gap-6 justify-center uppercase font-medium text-gray-500 opacity-75 text-sm">
+          <li className="hover:text-gray-700">
+            <a href="">Home</a>
+          </li>
+          <li className="hover:text-gray-700">
+            <a href="">Projects</a>
+          </li>
+          <li className="hover:text-gray-700">
+            <a href="">Skills</a>
+          </li>
+          <li className="hover:text-gray-700">
+            <a href="">Experience</a>
+          </li>
+          <li className="hover:text-gray-700">
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
