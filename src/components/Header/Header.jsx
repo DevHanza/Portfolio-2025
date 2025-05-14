c91db2612429ed97c5f234311f1f0e0f308bc6aa
DevHanza <hansana123dasanayake@gmail.com>
2025-05-14 21:30:14 +0530

    Create Header With Basic Structure (No Interactions)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,33 @@
+import "./Header.css";
+
 function Header() {
   return (
-    <header className="h-22 flex items-center justify-center">
-      <nav>
-        <ul className="flex gap-6 justify-center uppercase font-medium text-gray-500 opacity-75 text-sm">
+    <header className="flex h-auto flex-col items-center justify-center bg-white md:h-22">
+      <div className="mobile-menu flex w-full justify-between p-4 md:hidden">
+        <span className="menu-title">HansanaPrabath</span>
+
+        <div className="menu-btn">
+          <input id="menu-toggle" type="checkbox" hidden />
+          <label htmlFor="menu-toggle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              fill={"currentColor"}
+              viewBox="0 0 24 24"
+            >
+              {/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}
+              <path d="M4 4.08H20V6.08H4z"></path>
+              <path d="M4 11H20V13H4z"></path>
+              <path d="M4 17.92H20V19.92H4z"></path>
+            </svg>
+          </label>
+        </div>
+      </div>
+
+      {/* Nav List */}
+      <nav className="nav-links">
+        <ul className="flex flex-col justify-end gap-6 text-sm font-medium text-gray-500 uppercase opacity-75 md:flex-row md:justify-center">
           <li className="hover:text-gray-700">
             <a href="">Home</a>
           </li>
