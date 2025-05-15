40bcc366b31bee7af4e1b974b727f81470012123
DevHanza <hansana123dasanayake@gmail.com>
2025-05-15 21:25:09 +0530

    Add Mobile Menu Open & Close Function + Transition

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,46 +4,40 @@ import "./Header.css";
 function Header() {
   const [isHeaderVisible, setIsHeaderVisible] = useState(false);
 
-  const toggleMenu = (e) => {
-    if (e.target.checked) {
-      setIsHeaderVisible(true);
-    } else {
-      setIsHeaderVisible(false);
-    }
+  const toggleMenu = () => {
+    setIsHeaderVisible(!isHeaderVisible);
   };
 
   return (
     <header className="flex h-auto flex-col items-center justify-center bg-white md:h-22">
       <div className="mobile-menu flex w-full justify-between p-4 md:hidden">
-        <span className="menu-title">HansanaPrabath</span>
+        <span className="menu-title content-center font-medium text-[var(--primary-dark-blue)]">
+          HansanaPrabath
+        </span>
 
-        <div className="menu-btn bg-blue-200 p-2">
-          <input
-            id="menu-toggle"
-            className=""
-            type="checkbox"
-            onChange={toggleMenu}
-            hidden
-          />
-          <label htmlFor="menu-toggle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              fill={"currentColor"}
-              viewBox="0 0 24 24"
-            >
-              {/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}
-              <path d="M4 4.08H20V6.08H4z"></path>
-              <path d="M4 11H20V13H4z"></path>
-              <path d="M4 17.92H20V19.92H4z"></path>
-            </svg>
-          </label>
-        </div>
+        <button
+          className="menu-btn cursor-pointer bg-blue-200 p-2"
+          onClick={toggleMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            fill={"currentColor"}
+            viewBox="0 0 24 24"
+          >
+            {/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}
+            <path d="M4 4.08H20V6.08H4z"></path>
+            <path d="M4 11H20V13H4z"></path>
+            <path d="M4 17.92H20V19.92H4z"></path>
+          </svg>
+        </button>
       </div>
 
       {/* Nav List */}
-      <nav className={`${isHeaderVisible ? "flex" : "hidden"} nav-links`}>
+      <nav
+        className={`${isHeaderVisible ? "nav-visible" : "nav-invisible"} nav-links w-full justify-center bg-[var(--primary-dark-blue)]/5 md:flex md:w-auto md:bg-transparent`}
+      >
         <ul className="flex flex-col justify-end gap-6 text-center text-sm font-medium text-gray-500 uppercase opacity-75 md:flex-row md:justify-center">
           <li className="hover:text-gray-700">
             <a href="">Home</a>
