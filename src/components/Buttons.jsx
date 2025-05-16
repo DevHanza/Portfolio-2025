e0f3709944aa839c3c86624a23c9ce4a3b7241c0
DevHanza <hansana123dasanayake@gmail.com>
2025-05-16 21:59:14 +0530

    Add Button Component Styles For Primary & Secondary with Props

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,3 +1,9 @@
-export function Button({ children }) {
-  return <button>{children}</button>;
+export function Button({ type = "primary", children }) {
+  return (
+    <button
+      className={` ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} ${type === "secondary" ? "border-2 text-[var(--primary-dark-blue)]" : ""} hover:bg-[var(--primary-dark-blue) h-10 min-w-[130px] cursor-pointer rounded-sm px-6 text-base transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white md:h-11 md:min-w-[150px]`}
+    >
+      {children}
+    </button>
+  );
 }
