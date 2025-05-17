b721eb551e19e315e88cced67d21835eb222862b
DevHanza <hansana123dasanayake@gmail.com>
2025-05-17 20:50:06 +0530

    Primary & Secondary Button Component Styles, Dynamic Load with Props

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,12 @@
 export function Button({ type = "primary", children }) {
   return (
     <button
-      className={` ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} ${type === "secondary" ? "border-2 text-[var(--primary-dark-blue)]" : ""} hover:bg-[var(--primary-dark-blue) h-10 min-w-[130px] cursor-pointer rounded-sm px-6 text-base transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white md:h-11 md:min-w-[150px]`}
+      className={` 
+        
+        ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} 
+        ${type === "secondary" ? "border-2 text-[var(--primary-dark-blue)]" : ""} 
+
+        hover:bg-[var(--primary-dark-blue) h-10 min-w-[130px] cursor-pointer rounded-sm px-6 text-base transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white md:h-11 md:min-w-[150px]`}
     >
       {children}
     </button>
