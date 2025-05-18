966db527cf524b35acf78198e53eb0fe868ae9c7
DevHanza <hansana123dasanayake@gmail.com>
2025-05-18 16:37:17 +0530

    Add Containers to Header & Hero in a Proper way

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -10,45 +10,47 @@ function Header() {
   };
 
   return (
-    <header className="container mx-auto flex h-auto flex-col items-center justify-center bg-white md:h-22">
-      <div className="mobile-menu flex w-full justify-between px-4 py-2 md:hidden">
-        <span className="menu-title content-center font-medium text-[var(--primary-dark-blue)]">
-          <a href="/">HansanaPrabath</a>
-        </span>
+    <header className="flex h-auto flex-col items-center justify-center bg-white md:h-22">
+      <div className="container mx-auto">
+        <div className="mobile-menu flex w-full justify-between px-4 py-2 md:hidden">
+          <span className="menu-title content-center font-medium text-[var(--primary-dark-blue)]">
+            <a href="/">HansanaPrabath</a>
+          </span>
 
-        <button
-          className="menu-btn cursor-pointer rounded-md bg-stone-100 p-1 text-[var(--primary-dark-blue)] hover:bg-[var(--primary-dark-blue)]/10"
-          onClick={toggleMenu}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            fill={"currentColor"}
-            viewBox="0 0 24 24"
+          <button
+            className="menu-btn cursor-pointer rounded-md bg-stone-100 p-1 text-[var(--primary-dark-blue)] hover:bg-[var(--primary-dark-blue)]/10"
+            onClick={toggleMenu}
           >
-            {/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}
-            <path d="M4 4.08H20V6.08H4z"></path>
-            <path d="M4 11H20V13H4z"></path>
-            <path d="M4 17.92H20V19.92H4z"></path>
-          </svg>
-        </button>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              fill={"currentColor"}
+              viewBox="0 0 24 24"
+            >
+              {/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}
+              <path d="M4 4.08H20V6.08H4z"></path>
+              <path d="M4 11H20V13H4z"></path>
+              <path d="M4 17.92H20V19.92H4z"></path>
+            </svg>
+          </button>
+        </div>
 
-      {/* Nav List */}
-      <nav
-        className={`${isHeaderVisible ? "nav-visible" : "nav-invisible"} nav-links w-full justify-center bg-stone-100 md:flex md:max-h-25! md:w-auto md:bg-transparent`}
-      >
-        <ul className="flex flex-col justify-end gap-6 text-center text-sm font-medium text-gray-500 uppercase opacity-75 md:flex-row md:justify-center">
-          {NavItems.map((item) => {
-            return (
-              <li key={item.linkName} className="hover:text-gray-700">
-                <a href={item.url}>{item.linkName}</a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+        {/* Nav List */}
+        <nav
+          className={`${isHeaderVisible ? "nav-visible" : "nav-invisible"} nav-links w-full justify-center bg-stone-100 md:flex md:max-h-25! md:w-auto md:bg-transparent`}
+        >
+          <ul className="flex flex-col justify-end gap-6 text-center text-sm font-medium text-gray-500 uppercase opacity-75 md:flex-row md:justify-center">
+            {NavItems.map((item) => {
+              return (
+                <li key={item.linkName} className="hover:text-gray-700">
+                  <a href={item.url}>{item.linkName}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,28 +2,30 @@ import { Button } from "../../components/Buttons";
 
 function Hero() {
   return (
-    <section id="hero" className="section container mx-auto">
-      <div className="hero-content text-center">
-        <h1 className="hero-text mb-1 text-4xl font-semibold tracking-tight md:text-6xl lg:text-7xl">
-          Hansana Prabath
-        </h1>
-        <p className="mb-4.5 text-xl md:text-3xl lg:text-4xl">
-          Frontend Engineer
-        </p>
+    <section id="hero" className="section">
+      <div className="container mx-auto">
+        <div className="hero-content text-center">
+          <h1 className="hero-text mb-1 text-4xl font-semibold tracking-tight md:text-6xl lg:text-7xl">
+            Hansana Prabath
+          </h1>
+          <p className="mb-4.5 text-xl md:text-3xl lg:text-4xl">
+            Frontend Engineer
+          </p>
 
-        <p className="mx-auto mb-9 max-w-[85%] text-base tracking-tight text-[var(--primary-dark-blue)]/75 md:max-w-110 md:text-lg">
-          Here’s a look at some of the experiences that have shaped me into the
-          developer I am today. 🇱🇰
-        </p>
+          <p className="mx-auto mb-9 max-w-[85%] text-base tracking-tight text-[var(--primary-dark-blue)]/75 md:max-w-110 md:text-lg">
+            Here’s a look at some of the experiences that have shaped me into
+            the developer I am today. 🇱🇰
+          </p>
 
-        <div className="inline-flex flex-wrap justify-center gap-2">
-          <Button type="primary">Hire Me</Button>
-          <Button type="secondary">See Work</Button>
+          <div className="inline-flex flex-wrap justify-center gap-2">
+            <Button type="primary">Hire Me</Button>
+            <Button type="secondary">See Work</Button>
+          </div>
         </div>
-      </div>
 
-      <div className="hero-image mt-4.5 flex justify-center">
-        <img src="./hanza-blurred.png" alt="Portrait Image of Hansana." />
+        <div className="hero-image mt-4.5 flex justify-center">
+          <img src="./hanza-blurred.png" alt="Portrait Image of Hansana." />
+        </div>
       </div>
     </section>
   );
