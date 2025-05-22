20329586e96049de62eeae6b32d5543d429d69d9
DevHanza <hansana123dasanayake@gmail.com>
2025-05-22 21:43:30 +0530

    Develop "Section Label" Component

---
+++ src/components/SectionLabel.jsx
@@ -0,0 +1,9 @@
+function SectionLabel() {
+  return (
+    <span className="cursor-pointer rounded-full border-2 border-[var(--primary-blue)]/90 bg-[var(--primary-blue)]/10 px-4 py-1 text-sm font-medium text-[var(--primary-blue)]/90 uppercase">
+      projects
+    </span>
+  );
+}
+
+export default SectionLabel;
