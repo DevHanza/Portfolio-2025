4e60c0993b5604fc83fe801db8cd2c825164a959
DevHanza <hansana123dasanayake@gmail.com>
2025-05-23 13:46:13 +0530

    Change the opacity of Section label borde

--- src/components/SectionLabel.jsx
+++ src/components/SectionLabel.jsx
@@ -1,6 +1,6 @@
 function SectionLabel({ text = "Text" }) {
   return (
-    <div className="inline-block leading-tight cursor-pointer rounded-full border-1 border-[var(--primary-blue)]/90 bg-[var(--primary-blue)]/10 px-4 py-1 text-xs md:text-sm font-medium text-[var(--primary-blue)]/90 uppercase">
+    <div className="inline-block leading-tight cursor-pointer rounded-full border-1 border-[var(--primary-blue)]/40 bg-[var(--primary-blue)]/10 px-4 py-1 text-xs md:text-sm font-medium text-[var(--primary-blue)]/90 uppercase">
       <span className="leading-tight align-middle">{text}</span>
     </div>
   );
