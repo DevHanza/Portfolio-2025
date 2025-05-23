ec18746a1e7252b8adf2d06d52dda24a166fd45d
DevHanza <hansana123dasanayake@gmail.com>
2025-05-23 06:16:43 +0530

    Fix SectionHeading Mobile Responsiveness

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -1,13 +1,18 @@
 import SectionLabel from "./SectionLabel";
 
-function SectionHeading() {
+function SectionHeading({
+  label = "Label",
+  title = "Your title goes here",
+  title_text = "Your Text goes here.",
+}) {
   return (
-    <div className="text-center max-w-[500px]">
-      <SectionLabel />
-      <h2 className="text-2xl md:text-3xl lg:text-4xl tracking-tight font-semibold pt-4">Work I'm Proud Of</h2>
-      <p className="pt-2 tracking-tight text-[var(--primary-dark-blue)]/75 font-normal">
-        Here's a peek at some of the projects I've built with love, curiosity,
-        and a lot of coffee.
+    <div className="mx-auto max-w-[500px] text-center">
+      <SectionLabel text={label} />
+      <h2 className="pt-4 text-2xl font-semibold tracking-tight md:text-3xl lg:pt-4 lg:text-4xl">
+        {title}
+      </h2>
+      <p className="pt-1 font-normal tracking-tight text-[var(--primary-dark-blue)]/75 lg:pt-2">
+        {title_text}
       </p>
     </div>
   );
