92dcfc40c0156a91752a298f6574e15fffd348fe
DevHanza <hansana123dasanayake@gmail.com>
2025-05-23 17:39:43 +0530

    Buttons.jsx : indent code for better readability

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,10 @@ export function Button({ href = "#", type = "primary", children }) {
   return (
     <a href={href}>
       <button
-        className={` ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} ${type === "secondary" ? "text-[var(--primary-dark-blue)]" : ""} hover:bg-[var(--primary-dark-blue) h-10 min-w-[130px] cursor-pointer rounded-sm border-2 border-[var(--primary-dark-blue)] px-6 text-base transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white md:h-11 md:min-w-[150px]`}
+        className={` 
+          ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} 
+          ${type === "secondary" ? "text-[var(--primary-dark-blue)]" : ""} 
+          hover:bg-[var(--primary-dark-blue) h-10 min-w-[130px] cursor-pointer rounded-sm border-2 border-[var(--primary-dark-blue)] px-6 text-base transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white md:h-11 md:min-w-[150px]`}
       >
         {children}
       </button>
