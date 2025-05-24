8e58f2f2d7210d72874350dcf2f5ab327adaa4bc
DevHanza <hansana123dasanayake@gmail.com>
2025-05-24 16:06:39 +0530

    Make MiniProjectCard Responsive & More Styles

--- src/components/ProjectCards/Mini-Card/MiniProjectCard.jsx
+++ src/components/ProjectCards/Mini-Card/MiniProjectCard.jsx
@@ -1,13 +1,15 @@
+import "./MiniProjectCard.css";
+
 function MiniProjectsCard({
   title = "Calculator",
   ghLink = "https://github.com",
   imgSrc = "./Project-Card-Image.png",
 }) {
   return (
-    <div className="relative h-35 w-full overflow-hidden rounded-lg">
-      <a href={ghLink}>
+    <div className="mini-project-card relative flex h-35 w-full flex-row overflow-hidden rounded-lg">
+      <a href={ghLink} className="h-full w-full">
         {/* Mini-Projects-Card */}
-        <img src={imgSrc} alt="" className="h-full w-full object-cover" />
+        <img src={imgSrc} alt={title} className="h-full w-full object-cover" />
 
         <div className="absolute bottom-0 left-0 z-20 mb-3 ml-3 flex gap-2">
           <span>
