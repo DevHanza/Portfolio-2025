6d222f684cb24ee9e82e6e370eecea95c3a7fbad
DevHanza <hansana123dasanayake@gmail.com>
2025-05-24 16:05:28 +0530

    Make the MiniProjectsCardSlider Responsive

--- src/components/ProjectCards/MiniProjectsCardSlider.jsx
+++ src/components/ProjectCards/MiniProjectsCardSlider.jsx
@@ -1,13 +1,31 @@
-import MiniProjectsCard from "./Mini-Card/Mini-Project-Card";
+import ChevronLeftIcon from "../../assets/svg/chevron-left";
+import ChevronRightIcon from "../../assets/svg/chevron-right";
+import MiniProjectsCard from "./Mini-Card/MiniProjectCard";
 
 function MiniProjectsCardSlider() {
   return (
-    <div className="">
-      <div className="flex flex-col gap-3 md:flex-row md:gap-4 lg:gap-5">
+    <div className="relative select-none">
+      <div className="absolute top-1/2 left-0 z-100 inline-block -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full bg-gray-50 p-1 shadow-md transition-all duration-150 ease-in-out hover:bg-gray-100 hover:shadow-none">
+        <ChevronLeftIcon
+          height="28"
+          width="28"
+          className="fill-[var(--primary-dark-blue)]"
+        />
+      </div>
+
+      <div className="flex flex-wrap gap-1 md:flex-row lg:gap-2">
         <MiniProjectsCard />
         <MiniProjectsCard />
         <MiniProjectsCard />
         <MiniProjectsCard />
+      </div>
+
+      <div className="absolute top-1/2 right-0 inline-block translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full bg-gray-50 p-1 shadow-md transition-all duration-150 ease-in-out hover:bg-gray-100 hover:shadow-none">
+        <ChevronRightIcon
+          height="28"
+          width="28"
+          className="fill-[var(--primary-dark-blue)]"
+        />
       </div>
     </div>
   );
