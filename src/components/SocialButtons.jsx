e40d9aa22c01bb044bf1a9c2974eb144e44427ea
DevHanza <hansana123dasanayake@gmail.com>
2025-05-25 09:34:12 +0530

    Add Color Prop to SocialButtons to change the color of icons

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -5,36 +5,40 @@ import StackOverflowIcon from "../assets/svg/stack-overflow";
 import MediumIcon from "../assets/svg/medium";
 import DevToIcon from "../assets/svg/dev-to";
 
-function SocialButtons({ classes, size = "h-5 md:h-6" }) {
+function SocialButtons({
+  classes,
+  size = "h-5 md:h-6",
+  color = "fill-[var(--primary-dark-blue)]",
+}) {
   return (
     <div className={`flex ${classes}`}>
       <HoverBtnWrapper>
         <a href="https://www.linkedin.com/in/devhanza/">
-          <LinkedinIcon className={size} />
+          <LinkedinIcon className={`${size} ${color}`} />
         </a>
       </HoverBtnWrapper>
 
       <HoverBtnWrapper>
         <a href="https://github.com/DevHanza">
-          <GithubIcon className={size} />
+          <GithubIcon className={`${size} ${color}`} />
         </a>
       </HoverBtnWrapper>
 
       <HoverBtnWrapper>
         <a href="https://stackoverflow.com/users/16218477/hansana-prabath">
-          <StackOverflowIcon className={size} />
+          <StackOverflowIcon className={`${size} ${color}`} />
         </a>
       </HoverBtnWrapper>
 
       <HoverBtnWrapper>
         <a href="https://medium.com/@devhanza">
-          <MediumIcon className={size} />
+          <MediumIcon className={`${size} ${color}`} />
         </a>
       </HoverBtnWrapper>
 
       <HoverBtnWrapper>
         <a href="https://dev.to/devhanza">
-          <DevToIcon className={size} />
+          <DevToIcon className={`${size} ${color}`} />
         </a>
       </HoverBtnWrapper>
     </div>
