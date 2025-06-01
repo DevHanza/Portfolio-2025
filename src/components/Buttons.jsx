916772f7dd16c5011c2f58e7f550de094ef29123
DevHanza <hansana123dasanayake@gmail.com>
2025-06-01 13:40:20 +0530

    Change Button Styles for New Design (New Design changes starts here!)

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,9 +3,9 @@ export function Button({ href = "#", type = "primary", children }) {
     <a href={href}>
       <button
         className={` 
-          ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} 
-          ${type === "secondary" ? "text-[var(--primary-dark-blue)]" : ""} 
-          hover:bg-[var(--primary-dark-blue) h-10 min-w-[130px] cursor-pointer rounded-sm border-2 border-[var(--primary-dark-blue)] px-6 text-base transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white md:h-11 md:min-w-[150px]`}
+        ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} 
+        ${type === "secondary" ? "text-[var(--primary-dark-blue)]" : ""} 
+        hover:bg-[var(--primary-dark-blue) cursor-pointer rounded-sm border-1 border-[var(--primary-dark-blue)]/50 px-5 py-2 text-base font-medium transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white`}
       >
         {children}
       </button>
