bdeb17da3db3f521939ff0410cf28732c209b7a7
DevHanza <hansana123dasanayake@gmail.com>
2025-06-01 14:21:13 +0530

    Add Button feature to open Button links on new tab

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,13 @@
-export function Button({ href = "#", type = "primary", children }) {
+export function Button({
+  href = "#",
+  type = "primary",
+  children,
+  newtab = "false",
+}) {
   return (
-    <a href={href}>
+    <a href={href} target={newtab === "true" ? "_blank" : ""}>
       <button
-        className={` 
-        ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} 
-        ${type === "secondary" ? "text-[var(--primary-dark-blue)]" : ""} 
-        hover:bg-[var(--primary-dark-blue) cursor-pointer rounded-sm border-1 border-[var(--primary-dark-blue)]/50 px-5 py-2 text-base font-medium transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white`}
+        className={` ${type === "primary" ? "bg-[var(--primary-dark-blue)] text-white" : ""} ${type === "secondary" ? "text-[var(--primary-dark-blue)]" : ""} hover:bg-[var(--primary-dark-blue) cursor-pointer rounded-sm border-1 border-[var(--primary-dark-blue)]/50 px-5 py-2 text-base font-medium transition ease-in hover:bg-[var(--primary-dark-blue)] hover:text-white`}
       >
         {children}
       </button>
