573ea680c53ea785af4fca68e01189b663e111ac
DevHanza <hansana123dasanayake@gmail.com>
2025-06-07 16:36:12 +0530

    Add CTA Section

--- src/sections/CTA/CTA.jsx
+++ src/sections/CTA/CTA.jsx
@@ -1,5 +1,25 @@
+import "./CTA.css";
+import { Button } from "../../components/Buttons/Buttons";
 function CTA() {
-  return <div>CTA</div>;
+  return (
+    <section id="cta" className="section">
+      <div className="container mx-auto flex max-w-[var(--container-width)] flex-col gap-12">
+        <div className="cta min-h-25 rounded-3xl px-6 py-14 text-center">
+          <h2 className="mx-auto mb-4 max-w-60 text-2xl leading-tight font-semibold tracking-tight text-white md:max-w-xl lg:text-3xl">
+            Need a Developer You Can Count On?
+          </h2>
+          <p className="mx-auto mb-7 max-w-lg text-base font-light text-stone-200">
+            Whether you're hiring or starting a new project, I build fast,
+            modern websites that work beautifully and feel great to use.
+          </p>
+
+          <Button type="secondary" href="https://calendly.com" newtab="true">
+            Book a Free Call
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default CTA;
