692e838dfd5bb44c7362ce659ae112e5d9f6dac9
DevHanza <hansana123dasanayake@gmail.com>
2025-06-09 23:10:21 +0530

    Add FadeIn & SlideIn Transition Componenets

---
+++ src/transitions/FadeIn.jsx
@@ -0,0 +1,22 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export function FadeIn(target, options = {}) {
+  gsap.fromTo(
+    target,
+    { opacity: 0 },
+    {
+      opacity: 1,
+      duration: 0.8,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: target,
+        start: "top 90%",
+        toggleActions: "play none none none",
+      },
+      ease: "power3.out",
+      ...options,
+    },
+  );
+}
---
+++ src/transitions/SlideIn.jsx
@@ -0,0 +1,48 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export function SlideInUp(target, options = {}) {
+  gsap.fromTo(
+    target,
+    { y: "50%", opacity: 0, filter: "blur(8px)" },
+    {
+      x: 0,
+      y: 0,
+      filter: "blur(0px)",
+      opacity: 1,
+      duration: 0.8,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: target,
+        start: "top 90%",
+        toggleActions: "play none none none",
+      },
+      ease: "power3.out",
+      ...options,
+    },
+  );
+}
+
+export function SlideInRight(target, options = {}) {
+  gsap.fromTo(
+    target,
+    { x: "50%", opacity: 0, filter: "blur(8px)" },
+    {
+      x: 0,
+      y: 0,
+
+      filter: "blur(0px)",
+      opacity: 1,
+      duration: 0.8,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: target,
+        start: "top 90%",
+        toggleActions: "play none none none",
+      },
+      ease: "power3.out",
+      ...options,
+    },
+  );
+}
