b00a6d22858a031f400bf6a9b0ca603adb01b0cc
DevHanza <hansana123dasanayake@gmail.com>
2025-06-12 22:16:22 +0530

    fix slide in up animation trigger

--- src/transitions/Slide.jsx
+++ src/transitions/Slide.jsx
@@ -15,7 +15,7 @@ export function SlideInUp(target, scrollTriggerOptions = {}, options = {}) {
       stagger: 0.2,
       scrollTrigger: {
         trigger: target,
-        start: "top 40%",
+        start: "top 90%",
         toggleActions: "play none none none",
         ...scrollTriggerOptions,
       },
