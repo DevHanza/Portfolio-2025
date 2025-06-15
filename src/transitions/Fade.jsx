608d7bdf5bc99d6d09147368fd8340164e4c6767
DevHanza <hansana123dasanayake@gmail.com>
2025-06-15 14:46:33 +0530

    Refeactor Transitions (Fade & Slide)

--- src/transitions/Fade.jsx
+++ src/transitions/Fade.jsx
@@ -2,21 +2,11 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-export function FadeIn(target, scrollTriggerOptions = {}, options = {}) {
-  return gsap.fromTo(
-    target,
-    { opacity: 0 },
-    {
-      opacity: 1,
-      duration: 0.8,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: target,
-        start: "top 90%",
-        toggleActions: "play none none none",
-        ...scrollTriggerOptions,
-      },
-      ...options,
-    },
-  );
+export function FadeIn(target, options = {}) {
+  return gsap.from(target, {
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.2,
+    ...options,
+  });
 }
--- src/transitions/Slide.jsx
+++ src/transitions/Slide.jsx
@@ -2,46 +2,24 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-export function SlideInUp(target, scrollTriggerOptions = {}, options = {}) {
-  return gsap.fromTo(
-    target,
-    { y: "50%", opacity: 0, filter: "blur(8px)" },
-    {
-      x: 0,
-      y: 0,
-      filter: "blur(0px)",
-      opacity: 1,
-      duration: 0.8,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: target,
-        start: "top 90%",
-        toggleActions: "play none none none",
-        ...scrollTriggerOptions,
-      },
-      ...options,
-    },
-  );
+export function SlideInUp(target, options = {}) {
+  return gsap.from(target, {
+    y: "50%",
+    opacity: 0,
+    filter: "blur(8px)",
+    duration: 0.8,
+    stagger: 0.2,
+    ...options,
+  });
 }
 
-export function SlideInRight(target, scrollTriggerOptions = {}, options = {}) {
-  gsap.fromTo(
-    target,
-    { x: "50%", opacity: 0, filter: "blur(8px)" },
-    {
-      x: 0,
-      y: 0,
-      filter: "blur(0px)",
-      opacity: 1,
-      duration: 0.8,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: target,
-        start: "top 90%",
-        toggleActions: "play none none none",
-        ...scrollTriggerOptions,
-      },
-      ...options,
-    },
-  );
+export function SlideInRight(target, options = {}) {
+  gsap.from(target, {
+    x: "50%",
+    opacity: 0,
+    filter: "blur(8px)",
+    duration: 0.8,
+    stagger: 0.2,
+    ...options,
+  });
 }
